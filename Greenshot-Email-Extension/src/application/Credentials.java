ca2f259a4d3c175fb7e03c58d2d92a860d14047f
andy.moser <andy.moser@WS015.int.blessing.ch>
2019-01-03 18:54:42 +0100

    Credentials.java w/o BFH Credentials

---
+++ Greenshot-Email-Extension/src/application/Credentials.java
@@ -0,0 +1,14 @@
+package application;
+/**
+ * DO NOT COMMIT THIS FILE!
+ * @author andy.moser
+ *
+ */
+
+public class Credentials {
+
+	/**BFH Credentials**/
+	public static String username = "";
+	public static String password = "";
+
+}
