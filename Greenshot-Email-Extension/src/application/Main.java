2563df0280f5ce0dae7c49a8752b2a4d169e9aff
andy.moser <andy.moser@WS015.int.blessing.ch>
2019-01-04 18:04:46 +0100

    Stand 19-1-4

--- Greenshot-Email-Extension/src/application/Main.java
+++ Greenshot-Email-Extension/src/application/Main.java
@@ -2,6 +2,10 @@ package application;
 
 import java.io.File;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.converters.FileConverter;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,32 +15,57 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private String path;
 	private String[] args;
+
+	/**
+	 * JCommander style commandline parameters.
+	 */
+	public static class Args {
+		@Parameter(names = "-s", description = "source TPS file or folder containing TPS files.", converter = FileConverter.class, required = false)
+		private File sourceFile;
+		@Parameter(names = "-i", description = "displays TPS file information.")
+		private boolean info;
+		@Parameter(names = "-idx", description = "displays the record ids for the available indexes.")
+		private boolean index;
+		@Parameter(names = "-layout", description = "displays the file layout.")
+		private String text;
+		@Parameter(names = "-t", description = "transmits text.")
+		private boolean stackTraces;
+		@Parameter(names = "-sT", description = "target CSV file or folder to create CSV files in.", converter = FileConverter.class, required = false)
+		private File targetFile;
+
+	}
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
-			String path =  new File(Main.class.getProtectionDomain().getCodeSource().getLocation()
-				    .toURI()).getPath();
-			
+			Args params = new Args();
+			JCommander cmd = new JCommander(params);
+
+			this.path = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.setTitle("Environment Test");
 			alert.setHeaderText("This Program is spinning at:");
 			alert.setContentText(path);
 
 			alert.showAndWait();
-			
-			
-			
-			
-			
+
+			try {
+
+				System.out.println(params.info);
+				System.out.println(params.text);
+
+			} catch (Exception e) {
+				System.out.println("no command arguments passed");
+			}
+
 			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("screen.fxml"));
 
 			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
-			
-			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
@@ -45,7 +74,7 @@ public class Main extends Application {
 	}
 
 	public static void main(String[] args) {
-		
+
 		launch(args);
 	}
 }
